ee6fb61b1af109cb808aa6d919e5aafc22888dba
fatimaqara999 <fatimaqara3@gmail.com>
2026-07-02 23:03:48 +0400

    Tapsiriq5

--- src/Tapsiriq5.java
+++ src/Tapsiriq5.java
@@ -10,14 +10,9 @@ public class Tapsiriq5 {
         System.out.print("Bir ondalik eded (double) daxil edin: ");
         double ondalikDeyer = sc.nextDouble();
 
-        int daraldilmis = (int) ondalikDeyer;   // (int) LAZIMDIR! El ile cast
-        System.out.println("Narrowing neticesi: " + daraldilmis);  // mes: 9.87 -> 9
+        int daraldilmis = (int) ondalikDeyer;
+        System.out.println("Narrowing neticesi: " + daraldilmis);
 
-        // ===== IZAH (tapsiriq bunu isteyir) =====
-        // Narrowing-de ondalik hisse ITIR: (int) 9.87 = 9, 10 yox!
-        // Java casting yuvarlamir, sadece ondalig KESIR (atir).
-
-        // Scanner-i baglayiriq
         sc.close();
         // Narrowing-de ondaliq hisse itir: (int) 9.87 = 9, 10 yox!
         // Java casting yuvarlamir, sadece ondaligi kesir (atir).
